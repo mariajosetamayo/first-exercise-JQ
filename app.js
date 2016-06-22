7e8481c2cc7a20eeccd94058de8797f455c24ca7
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2016-06-22 17:27:28 -0600

    JQ first exercise

---
+++ app.js
@@ -0,0 +1,21 @@
+
+var main = function() {
+ 
+//.keyup function allows to enter text and images by user
+
+ $("#top-text").keyup(function(){
+   var toAdd = $("#top-text").val();
+   $(".top-caption").text(toAdd);
+ });
+ $("#bottom-text").keyup(function(){
+   var toAdd= $("#bottom-text").val();
+   $(".bottom-caption").text(toAdd);
+ });
+  $("#image-url").keyup(function(){
+    var toAdd= $("#image-url").val();
+    $("img").attr("src", toAdd);
+  })
+  
+}
+ 
+$(document).ready(main);
